f8960c055afe087073a86924e1e1f0fbfeb62db6
Marta Banas <banas.m.marta@gmail.com>
2021-05-01 11:07:10 +0200

    pridano skryvani navigace + vycentrovano text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 
-console.log('funguju!');
+const navBtn = document.querySelector('.nav-btn');
+const nav = document.querySelector('nav');
+const navLinks = document.querySelectorAll('nav a');
+
+navBtn.addEventListener('click', () => {
+  nav.classList.toggle('nav-closed');
+});
+
+for (let navLink of navLinks) {
+  navLink.addEventListener('click', () => {
+    nav.classList.add('nav-closed');
+  });
+}
